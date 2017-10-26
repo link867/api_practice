779180df1eb42dbe878c84c59774d845e0417773
Ryan Brzezinski <ryan.brzezinski867@gmail.com>
2017-10-26 16:28:27 -0400

    Added getMovie func, now working

--- main.go
+++ main.go
@@ -1,54 +1,24 @@
-pckage main
+package main
 
 import (
     "encoding/json"
+    "io/ioutil"
     "log"
     "net/http"
     "github.com/gorilla/mux"
+    "fmt"
+    "strings"
+    "strconv"
 )
 
-// our main function
-func main() {
-    router := mux.NewRouter()
-    router.HandleFunc("/movies", GetAll).Methods("GET")
-    router.HandleFunc("/movies", Create).Methods("POST")
-    router.HandleFunc("/movies/{id}", GetMovie).Methods("GET")
-    router.HandleFunc("/movies/{id}", Update).Methods("PUT")
-    router.HandleFunc("/movies/{id}", DeletePerson).Methods("DELETE")
-    log.Fatal(http.ListenAndServe(":8080", router))
-}
-
-
-func GetAll(w http.ResponseWriter, r *http.Request) {
-
-	json.NewEncoder(w).Encode(Movies)
-}
-
-
-func GetMovie(w http.ResponseWriter, r *http.Request) {
-
-	par := mux.Vars(req);
-	for _, movie := range Movies {
-
-		if movie.ID == par["id"] {
-
-			json.NewEncoder(w).Encode(movie)
-
-			return
-
-		}
-
-	}
-
-	json.NewEncoder(w).Encode()
-}
-
-var Movies = []struct {
+type Movie struct {
      ID       int
      Director string
      Name     string
      Rating   int
-} {
+}
+
+var Movies = []Movie  {
     {
         ID: 1,
         Director: "Steven Spielberg",
@@ -73,4 +43,59 @@ var Movies = []struct {
         Name: "Transformers 3",
         Rating: 3,
     },
+}
+
+
+
+
+
+func GetAll(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("Returning all movies")
+
+	json.NewEncoder(w).Encode(Movies)
+
+	return
+}
+
+
+func GetMovie(w http.ResponseWriter, r *http.Request) {
+
+	u := strings.Split(r.URL.Path, "/")
+	i, _ := strconv.Atoi(u[2])
+	m := Movies[i - 1]
+	json.NewEncoder(w).Encode(m)
+}
+
+
+func Create(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+	//	panic(err)
+		fmt.Println("Error readying body: %v", err)
+	}
+
+	var m Movie
+	fmt.Println("body:", body)
+	err = json.Unmarshal(body, &m)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+}
+
+
+
+func main() {
+    router := mux.NewRouter()
+    router.HandleFunc("/movies", GetAll).Methods("GET")
+    router.HandleFunc("/movies", Create).Methods("POST")
+    router.HandleFunc("/movies/{id}", GetMovie).Methods("GET")
+    router.HandleFunc("/movies/{id}", Update).Methods("PUT")
+    router.HandleFunc("/movies/{id}", Delete).Methods("DELETE")
+    log.Fatal(http.ListenAndServe(":8080", router))
 }
